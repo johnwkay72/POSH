d636310c9ce207ce1bcc988b32677a58656f0f08
John W Kay <38020072+johnwkay72@users.noreply.github.com>
2020-02-25 18:59:45 -0500

    Update ESP_Get-OU_info.ps1

--- ESP_Get-OU_info.ps1
+++ ESP_Get-OU_info.ps1
@@ -26,88 +26,24 @@
 #region - SET THE ENVIRONMENT / SMTP SERVER.
     $FQD = (Get-WmiObject win32_computersystem).Domain ;
     
-    if ($FQD -like 'tws.com') 
+    if ($FQD -like 'dev.com') 
         {
         $domain='DEV'
         $exoENV='DEV'
-        # $smtpFROM='EZM-PROD@exostar.com'
-        # $smtp='10.36.9.9'
-        $baseURL = "https://mysite.dcapp23.tws.com"
+        $smtpFROM='APP-ENV@domain.com'
+        $smtp='10.10.1.x'
+        $baseURL = "https://SPsite.ENV.domain.com"
         }
-    elseif ($FQD -like 'MPF.exostar.com') 
+    if ($FQD -like 'site01.domain.com') 
         {
-        $domain='MPF'
-        $exoENV='EZM-PROD'
-        $exoDC=''
-        $smtpFROM='EZM-PROD@exostar.com'
-        $smtp='10.36.9.9'
-        $baseURL = "https://mysite.mpfqa.exostartest.com"
+        $domain='ENV'
+        $exoENV='APP-ENV'
+        $exoDC='tbd'
+        $smtpFROM='APP-ENV@domain.com'
+        $smtp='10.10.2.x'
+        $baseURL = "https://SPsite.ENV.domain.com"
         }
-    elseif ($FQD -like "FPX.exostar.com") 
-        {
-        $domain='FPX'
-        $exoENV='FPv6-US'
-        $smtpFROM='FPv6-US@exostar.com'
-        $smtp='10.36.9.9'
-        # $baseURL = "https://mysite.mpfqa.exostartest.com"
-        }
-    elseif ($FQD -like "fpx-mpi.exostar.com") 
-        {
-        $domain='FPK-MPI'
-        $exoENV='FPv6-UK'
-        $smtpFROM='FPv6UK@exostar.com'
-        $smtp='10.38.19.6'
-        # $baseURL = "https://mysite.mpfqa.exostartest.com"
-        }
-    elseif ($FQD -like "FPX1.exostartest.com") 
-        {
-        $domain='FPX1'
-        $exoENV='FPv6-UAT'
-        $smtpFROM='FPv6-UAT@exostar.com'
-        $smtp='10.248.3.4'
-        # $baseURL = "https://mysite.mpfqa.exostartest.com"
-        }
-    elseif ($FQD -like "test.fp.local") 
-        {
-        $domain='TEST'
-        $exoENV='FPv7-QA'
-        $smtpFROM='FPv7-QA@exostar.com'
-        $smtp='10.248.3.4'
-        $baseURL = "https://main.fpqa.exostartest.com"
-        }
-    elseif ($FQD -like "uat.fp.local") 
-        {
-        $domain='UAT'
-        $exoENV='FPv7-UAT'
-        $smtpFROM='FPv7-UAT@exostar.com'
-        $smtp='ns1.exostartest.com'
-        $baseURL = "https://main.fpuat.exostartest.com"
-        }
-    elseif ($FQD -like "fpx.exostar.com") 
-        {
-        $domain='FPX'
-        $exoENV='FPv7-PROD'
-        $smtpFROM='FPv7-PRD@exostar.com'
-        $smtp='10.36.9.9'
-        $baseURL = "https://main.fp7s.exostar.com"
-        }
-    elseif ($FQD -like "MPF.exostartest.com") 
-        {
-        $domain='MPF'
-        $exoENV='EZM-UAT'
-        $smtpFROM='EZM-UAT@exostar.com'
-        $smtp='10.248.3.4'
-        $baseURL = "https://mysite.mpf.exostartest.com"
-        }
-    elseif ($FQD -like "MPFQA.exostartest.com") 
-        {
-        $domain='MPFQA'
-        $exoENV='EZM-QA'
-        $smtpFROM='EZM-QA@exostar.com'
-        $smtp='10.248.3.4'
-        $baseURL="https://mysite.mpfqa.exostartest.com"
-        }
-
+    
 #endregion
 # --------------------------------------------------------------------------- #
 #region - SET VARIABLES & ARRAYS:
@@ -116,7 +52,7 @@
     $line = "# --------------------------------------------------------------------------- #" ;
     $pad = "60" ;
     $smtp_regex = "(^[A-Za-z0-9]+)(.)([A-Za-z0-9]+)(@)(exostar.com)" ;
-    $outPATH = "C:\TECHOPS\SCRIPTS\OUTPUT" ;
+    $outPATH = "C:\path\SCRIPTS\OUTPUT" ;
     
     # CORRELATIONID REGEX:
     #   SAMPLE: F0BB0790-4323-A153-096F-ABCDC80E24D4
